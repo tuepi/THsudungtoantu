18c47418ca63a70719a57b0d10bfcb5aa6014621
Tuệ Trần Đình <trandinhtue140398@gmail.com>
2022-03-08 22:02:31 +0700

    Xong thực hành sử dụng toán tử

---
+++ toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều rộng");
+inputHeight = prompt("Nhâp chiều dài");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích là: " + area);
